293089dad543d909a719df460abbf68f38d69566
rheinsullivan <rheinsullivan06@gmail.com>
2024-09-17 11:15:25 +0700

    gk tau

--- src/pages/Khodam.vue
+++ src/pages/Khodam.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from "vue";
+import SayangnyaAku from "../assets/image/zahra-sayang.jpeg";
 
 const khodamData = [
   "Khodam Senyum Bahagia",
@@ -47,6 +48,8 @@ const specialNames = {
 const inputKhodam = ref("");
 const hasilKhodammu = ref("");
 const cheked = {};
+const imageSrc = ref("https://i.pinimg.com/564x/08/d5/21/08d521ece636dc2448bf7e142c85cc72.jpg");
+
 const removeEmoji = (name) => name.replace(/[\u{1F600}-\u{1F64F}]/gu, "").trim();
 
 function getRandomKhodam(khodamData) {
@@ -63,13 +66,15 @@ const handleClick = () => {
 
   const nameWithoutEmoji = removeEmoji(name);
   if (specialNames[nameWithoutEmoji]) {
-    hasilKhodammu.value = `Khodam kamu adalah: ${specialNames[nameWithoutEmoji]}`;
+    hasilKhodammu.value = `Pawang kamu adalah: ${specialNames[nameWithoutEmoji]}`;
+    imageSrc.value = SayangnyaAku;
   } else {
     if (!cheked[name]) {
       const khodamNew = getRandomKhodam(khodamData);
       cheked[name] = khodamNew;
     }
     hasilKhodammu.value = `Khodam kamu adalah: ${cheked[name]}`;
+    imageSrc.value = "https://i.pinimg.com/564x/08/d5/21/08d521ece636dc2448bf7e142c85cc72.jpg";
   }
 };
 </script>
@@ -84,25 +89,24 @@ const handleClick = () => {
             <h2 class="text-2xl sm:text-4xl md:text-5xl text-white font-bold mb-4 mt-5 md:mt-0">Cek Khodam <span class="text-red">Kamu</span></h2>
             <p class="text-xs md:text-base text-white md:w-full">Jika kamu gabut, coba masukkan namamu di bawah, untuk mengecek khodam pada diri kamu</p>
           </div>
-          <div class="block w-36 md:hidden -mr-5 md:mr-0">
-            <img src="https://i.pinimg.com/564x/08/d5/21/08d521ece636dc2448bf7e142c85cc72.jpg" class="w-3/4 rounded-t-full" alt="" />
+          <div class="block w-48 md:hidden -mr-10 md:mr-0">
+            <img :src="imageSrc" class="w-3/4 rounded-t-full" alt="" />
           </div>
         </div>
         <!-- Form Khodam -->
         <form class="mt-14 w-full" @submit.prevent="handleClick">
-          <div id="hasilKhodammu" class="h-16 w-full items-center flex md:h-14 md:w-[55%]">
+          <div id="hasilKhodammu" class="h-12 w-full items-center flex md:h-14 md:w-[55%]">
             <input id="inputKhodam" v-model="inputKhodam" type="text" class="flex-grow rounded-l-lg text-sm px-3 h-full md:px-6 md:text-lg focus:outline-none" placeholder="Masukkan namamu untuk mengecek isi Khodam dalam dirimu" />
             <button id="btnKhodam" type="submit" class="h-full px-3 lg:px-6 text-sm md:text-base text-white font-bold bg-red rounded-r-lg hover:bg-vueDark hover:text-black">Kirim</button>
           </div>
           <!-- Hasil Khodam -->
-          <div v-if="hasilKhodammu" class="block p-2 rounded-md bg-white mt-5 w-96">
+          <div v-if="hasilKhodammu" class="block text-xs p-2 rounded-md bg-slate-300 mt-5 md:text-base lg:w-96">
             {{ hasilKhodammu }}
           </div>
         </form>
       </div>
-      <!-- Images -->
       <div class="absolute hidden -right-16 bottom-24 w-1/3 md:block">
-        <img src="https://i.pinimg.com/564x/08/d5/21/08d521ece636dc2448bf7e142c85cc72.jpg" class="w-3/4 rounded-t-full" alt="" />
+        <img :src="imageSrc" class="w-3/4 rounded-t-full" alt="" />
       </div>
     </div>
   </section>
